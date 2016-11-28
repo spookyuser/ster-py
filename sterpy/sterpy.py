b403bebd5c6ee7ae34ed48f32ac6e4cf4d4ca02b
spookyuser <spookyUnknownUser@users.noreply.github.com>
2016-11-28 13:57:26 +0200

    Removed old xml methods

--- sterpy/sterpy.py
+++ sterpy/sterpy.py
@@ -1,23 +1,14 @@
-import os
 import time
 import string
 import omdb
 import socket
-import xml.etree.cElementTree as ElementTree
 import click
-import urllib2
-import urllib
 import webbrowser
 import requests
-import time as timelib
 
 __VERSION__ = '1.2.0'
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 cinema_array = []
-save_directory = click.get_app_dir('sterpy', roaming=False)
-movies_location = save_directory + "\\movies.xml"
-cinema_location = save_directory + "\\cinemas.xml"
-hash_location = save_directory + "\\hash.xml"
 
 
 class MovieObject:
@@ -57,63 +48,6 @@ def is_connected():
     except:
         pass
     return False
-
-
-def xml_parse_movie():
-    movies_array = []
-    movie_name = ' '
-    coming_soon = 0
-    movie_tags = []
-    with open(movies_location, 'rt') as f:
-        tree = ElementTree.parse(f)
-        for item in tree.findall('item'):
-            for node in item.getchildren():
-                if node.tag == 'id':
-                    movie_id = node.text
-                if node.tag == 'name':
-                    movie_name = node.text.lstrip().rstrip()
-                    if get_tags(movie_name) is not None:
-                        movie_tags, movie_name = get_tags(movie_name)
-                    else:
-                        movie_tags = None
-                if node.tag == 'coming_soon':
-                    if node.text == '1':
-                        coming_soon = 1
-                if node.tag == 'cinema_ids':
-                    cinema_id_array = node.text.split(',')
-                    if coming_soon != 1:
-                        movie = MovieObject(string.capwords(movie_name), movie_id, cinema_id_array, movie_tags, None)
-                        movies_array.append(movie)
-                        coming_soon = 0
-                    else:
-                        coming_soon = 0
-                else:
-                    pass
-        return movies_array
-
-
-def xml_parse_cinema():
-    global cinema_array
-    cinema_id = ''
-    cinema_province_id = ''
-    cinema_province_name = ''
-    with open(cinema_location, 'rt') as f:
-        tree = ElementTree.parse(f)
-    for province in tree.iter(tag='item'):
-        for node in province.getchildren():
-            if node.tag == 'id':
-                cinema_province_id = node.text
-            if node.tag == 'name':
-                cinema_province_name = node.text
-        for cinema in province:
-            for cine_num in cinema.getchildren():
-                for node in cine_num.getchildren():
-                    if node.tag == 'complexid':
-                        cinema_id = node.text
-                    if node.tag == 'name':
-                        cinema_name = node.text
-                        cinema = CinemaObject(cinema_name, cinema_id, cinema_province_id, cinema_province_name)
-                        cinema_array.append(cinema)
 
 
 def json_parse_cinema():
@@ -292,89 +226,6 @@ def display_choice(pairs, found_cinema):
             webbrowser.open("https://www.google.com/search?q=%s" % movie.n)
         elif command == 'TRAILER':
             webbrowser.open(movie.v, new=0, autoraise=True)
-
-
-def get_tags(word):
-    # TODO Dictionary of tags ASAP
-    tags = []
-    if word.find('IMAX 3D - ') > -1:
-        tags.append('IMAX')
-        tags.append('3D')
-        stripped_word = word.replace('IMAX 3D - ', '')
-        return tags, stripped_word
-    if word.find('IMAX') > -1:
-        tags.append('IMAX')
-        stripped_word = word.replace('IMAX - ', '')
-        return tags, stripped_word
-    if word.find('3D') > -1:
-        tags.append('3D')
-        stripped_word = word.replace('3D - ', '')
-        return tags, stripped_word
-    else:
-        return None
-
-
-def get_performances(movie_id, cinema_id):
-    date = []
-    time = []
-    fp = urllib2.urlopen("http://www.sterkinekor.com/scripts/xml_feed.php?name=performance&movie_id=%s" % movie_id)
-    tree = ElementTree.parse(fp)
-    fp.close()
-    for elem in tree.iterfind('.//item[complex_id="%s"]/performances/*' % cinema_id):
-        for date_index, dates in enumerate(elem.getchildren()):
-            if dates.tag == 'date':
-                time.append(dates.text)
-            for index_times, times in enumerate(dates.iterfind('.//show_time')):
-                converted_time = timelib.strftime("%H:%M", timelib.strptime(times.text, '%H:%M:%S'))
-                time.append(converted_time)
-        date.append(time)
-        time = []
-    print ''
-    for index, day in enumerate(date):
-        print [index + 1], '--', day[0]
-        movie_times = ', '.join(map(str, date[index][1:]))
-        print click.style('\t' + str(movie_times), fg='green')
-    if click.confirm('Do you want to open the booking page?'):
-        # TODO Prevent autoplay
-        webbrowser.open("http://www.sterkinekor.com/#/book/%s" % movie_id, new=0, autoraise=True)
-        exit(0)
-
-
-def check_update_xml():
-    # TODO alternatively just download HASHcheckXML and compare that with old. This could update unnecessarily tho
-    online_movie_md5 = ''
-    local_movie_md5 = ''
-    if not os.path.isdir(save_directory):
-        os.makedirs(save_directory)
-    if not os.path.isfile(movies_location and hash_location and cinema_location):
-        print "Creating local xml source..."
-        download_new_files()
-    else:
-        with open(hash_location, 'rt') as f:
-            tree = ElementTree.parse(f)
-            for node in tree.iterfind('.//md5MovieXmlHash'):
-                local_movie_md5 = node.text
-        fp = urllib2.urlopen("http://www.sterkinekor.com/website/scripts/xml_feed.php?name=hashcheck_app_android_v2")
-        tree = ElementTree.parse(fp)
-        fp.close()
-        for node in tree.iterfind('.//md5MovieXmlHash'):
-            online_movie_md5 = node.text
-        if online_movie_md5 == local_movie_md5:
-            pass
-        else:
-            download_new_files()
-    return None
-
-
-def download_new_files():
-    # TODO implement POST here -- movies.sterkinekor.co.za/Browsing/QuickTickets/Cinemas
-    file_retrieve = urllib.URLopener()
-    file_retrieve.retrieve("http://www.sterkinekor.com/website/scripts/xml_feed.php?name=movies", movies_location)
-    file_retrieve.retrieve("http://www.sterkinekor.com/website/scripts/xml_feed.php?name=cinemas", cinema_location)
-    file_retrieve.retrieve("http://www.sterkinekor.com/website/scripts/xml_feed.php?name=hashcheck_app_android_v2",
-                           hash_location)
-    print click.style('Updated Movies', fg='green')
-    return None
 
 
 def imdb_search(movie_name):
